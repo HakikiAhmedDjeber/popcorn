326ad025c792eb63f9f7d8d9676bf3cc1e0c0d6e
HakikiAhmedDjeber <hakikiahmeddjeber@gmail.com>
2023-12-16 20:22:02 +0100

    use prototypes module to verify types of props

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import PropsTypes from "prop-types";
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -10,18 +10,39 @@ const starsContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
+StarRating.protoTypes = {
+  maxRating: PropsTypes.number,
+  color: PropsTypes.string,
+  size: PropsTypes.number,
+  className: PropsTypes.string,
+  messages: PropsTypes.array,
+  defaultRating: PropsTypes.number,
+  onSetRating: PropsTypes.func,
 };
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating = () => {},
+}) {
+  const textStyle = {
+    lineHeight: 1,
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
   function ratingHandler(rate) {
     setRating(rate);
+    onSetRating(rate);
   }
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starsContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -30,22 +51,27 @@ export default function StarRating({ maxRating = 5 }) {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIN={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating - 1] || messages[rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-};
-
-function Star({ onRate, full, onHoverIN, onHoverOut }) {
+function Star({ onRate, full, onHoverIN, onHoverOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
     <span
       role="button"
@@ -58,8 +84,8 @@ function Star({ onRate, full, onHoverIN, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -68,7 +94,7 @@ function Star({ onRate, full, onHoverIN, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="grey" onSetRating={setMovieRating} />
+      <p>this movie was rated by {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating size={30} color="blue" defaultRating={2} />
+    <Test />
   </React.StrictMode>
 );
